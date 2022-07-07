c02fdd8bdda4e43617b77b5e4539acb76cad0af2
mikk369 <mikk.ereline@vikk.ee>
2022-07-07 16:26:57 +0300

    button adding events and delete button

--- main.js
+++ main.js
@@ -1,65 +1,37 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("new-task-form");
-  const input = document.querySelector("new-task-input");
-  const list_el = document.querySelector("#tasks");
+//selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".add-event");
+const todoList = document.querySelector(".todo-list");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+todoButton.addEventListener("click", addTodo);
+//Event listeners
 
-    const task = input.value;
+//functions
+function addTodo(event) {
+  //prevent form from submiting
+  event.preventDefault();
+  //todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
 
-    if (!task) {
-      alert("Please fill out the task");
-      return;
-    }
-    const task_el = document.createElement("div");
-    task_el.classList.add("task");
+  //delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "delete";
+  deleteButton.classList.add("delete");
+  todoDiv.appendChild(deleteButton);
 
-    const task_content_el = document.createElement("div");
-    task_content_el.classList.add("content");
-    task_content_el.innerText = task;
-
-    task_el.appendChild(task_content_el);
-
-    const task_input_el = document.createElement("input");
-    task_input_el.classList.add("text");
-    task_content_el.type = "text";
-    task_content_el.setAttribute("readonly", "readonly");
-
-    task_content_el.appendChild(task_content_el);
-
-    const task_actions_el = document.createElement("div");
-    task_actions_el.classList.add("actions");
-
-    const task_edit_el = document.createElement("button");
-    task_edit_el.classList.add("edit");
-
-    const task_delete_el = document.createElement("button");
-    task_delete_el.classList.add("delete");
-    task_delete_el.innerHTML = "Delete";
-
-    task_actions_el.appendChild(task_edit_el);
-    task_actions_el.appendChild(task_delete_el);
-
-    task_el.appendChild(task_actions_el);
-
-    list_el.appendChild(task_el);
-
-    input.value = "";
-
-    task_edit_el.addEventListener("click", () => {
-      if (task_edit_el.innerText.toLocaleLowerCase() == "edit") {
-        task_input_el.removeAttribute("readonly");
-        task_input_el.focus();
-        task_edit_el.innerText = "Save";
-      } else {
-        task_input_el.setAttribute("readonly", "readonly");
-        task_edit_el.innerText = "Edit";
-      }
-    });
-
-    task_delete_el.addEventListener("click", () => {
-      list_el.removeChild(task_el);
-    });
-  });
-});
+  //edit button TODO:kirjutab eelmise nupu millegipärast üle
+  //   const editButton = document.createElement("button");
+  //   deleteButton.innerHTML = "edit";
+  //   deleteButton.classList.add("edit");
+  //   todoDiv.appendChild(editButton);
+  //append to list
+  todoList.appendChild(todoDiv);
+  //clear todoInput Value
+  todoInput.value = "";
+}
