1346761399f96ec709070c4bc23cb67f366f08fd
mikk369 <mikk.ereline@vikk.ee>
2022-07-11 21:37:51 +0300

    created elements

--- main.js
+++ main.js
@@ -1,37 +1,55 @@
 //selectors
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".add-event");
-const todoList = document.querySelector(".todo-list");
+const eventName = document.querySelector("#eventholder");
+const addButton = document.querySelector("#addEvent");
+const addDate = document.querySelector('input[type="date"]');
+const place = document.querySelector("#placeholder");
+const comments = document.querySelector("#comment");
+const eventList = document.querySelector(".event-container");
 
-todoButton.addEventListener("click", addTodo);
-//Event listeners
+//event listeners
+
+addButton.addEventListener("click", addEvents);
 
 //functions
-function addTodo(event) {
-  //prevent form from submiting
+function addEvents(event) {
+  //prevent page refreshing
   event.preventDefault();
-  //todo DIV
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-  //create LI
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
+  //Event Div
+  const eventDiv = document.createElement("div");
+  eventDiv.classList.add("event");
 
-  //delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "delete";
-  deleteButton.classList.add("delete");
-  todoDiv.appendChild(deleteButton);
+  // validate inputs
+  if (eventName.value === "") {
+    alert("Please input values"), (eventDiv = false);
+  }
+  //create h1
+  const newEventName = document.createElement("h1");
+  newEventName.innerText = eventName.value;
+  eventDiv.appendChild(newEventName);
+  eventList.appendChild(eventDiv);
 
-  //edit button TODO:kirjutab eelmise nupu millegipärast üle
-  //   const editButton = document.createElement("button");
-  //   deleteButton.innerHTML = "edit";
-  //   deleteButton.classList.add("edit");
-  //   todoDiv.appendChild(editButton);
-  //append to list
-  todoList.appendChild(todoDiv);
-  //clear todoInput Value
-  todoInput.value = "";
+  //date element
+  const newDate = document.createElement("h3");
+  newDate.innerText = addDate.value;
+  eventDiv.appendChild(newDate);
+  eventList.appendChild(newDate);
+
+  //create h2 place
+  const newEventPlace = document.createElement("h2");
+  newEventPlace.innerText = place.value;
+
+  eventDiv.appendChild(newEventPlace);
+  eventList.appendChild(newEventPlace);
+
+  //comment span element
+  const newComments = document.createElement("span");
+  newComments.innerText = comments.value;
+  eventDiv.appendChild(newComments);
+  eventList.appendChild(newComments);
+
+  //clears input
+  eventName.value = "";
+  place.value = "";
+  comments.value = "";
+  addDate.value = "";
 }
